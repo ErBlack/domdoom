8f1d68814b11624bb1acc181c32dabe49a597138
ertema <ertema@yandex-team.ru>
2017-10-02 22:56:28 +0300

    schedule

--- src/modules/Invitation.js
+++ src/modules/Invitation.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Invitation.css';
 import Timer from './Timer.js';
 
-import header from '../images/title.png';
+import header from '../images/text/title.png';
 
 const START_DATE = new Date('2017-10-14T15:00:00.000Z');
 const PLACE_URL = 'http://antimars.ru/';
@@ -21,6 +21,16 @@ export default class Invitation extends Component {
                 </div>
                 <div className="invintation_disclamer">
                     <abbr title="Union Aerospace Corporation">UAC</abbr> не несёт отвественности за вашу жизнь и здоровье на мероприятии. На случай внезапного портала в ад, можете прихватить <span draggable>дробовик</span>.
+                </div>
+                <div className="invintation_program">
+                    <ul>
+                        <li>18:00 Начало</li>
+                        <li>18:30 Ужин</li>
+                        <li>19:00 Настолки/Консоли</li>
+                        <li>20:00 Пьянство</li>
+                        <li>21:00 The Empire Strikes Back</li>
+                        <li>23:00 Encore</li>
+                    </ul>
                 </div>
             </div>
         );
